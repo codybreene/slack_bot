d01c59a2889e57e5b48b229a1fbd01f3a689200d
codybreene <codybreene@gmail.com>
2020-04-21 23:47:03 -0700

    add simple logic check for single phrase and DM capability

--- receive.py
+++ receive.py
@@ -1,17 +1,35 @@
+from slack import WebClient
+from slack.errors import SlackApiError
 import os
+import logging
+logging.basicConfig(level=logging.DEBUG)
 from flask import Flask, request, Response
+from slackeventsapi import SlackEventAdapter
 
 #instantiate app
 app = Flask(__name__)
 
-SLACK_WEBHOOK_SECRET = os.environ.get('SLACK_WEBHOOK_SECRET')
+events_adapter = SlackEventAdapter(
+    os.environ['SLACK_SIGNING_SECRET'], endpoint="/events", server=app)
+client = WebClient(os.environ['BOT_TOKEN'])
 
-@app.route('/slack', methods=['POST'])
-def inbound():
-  if request.form.get('token') == SLACK_WEBHOOK_SECRET:
-    channel = request.form.get('channel_name')
-    username = request.form.get('user_name')
-    text = request.form.get('text')
-    inbound = "User: " + username
+def send_message(id, message):
+  try:
+    response = client.chat_postMessage(
+      channel=id,
+      text=message
+    )
+  except SlackApiError as e:
+    assert e.response["error"]
 
+@events_adapter.on("message")
+def handle_message(event_data):
+  message = event_data["event"]
+  if "yolo" in message.get('text'):
+    user = message["user"]
+    message = "Hello %s!" % user
+    send_message(user, message)
 
+@app.route('/', methods=['GET'])
+def test():
+  return Response('It works!')
